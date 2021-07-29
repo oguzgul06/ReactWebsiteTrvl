4f38a8aa0b9293d57e7ef6d790c291fbd336727d
oguz gül <ogzgl95@outlook.com>
2021-07-30 00:05:07 +0300

    complated service and product pages

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "../../App.css";
+import Footer from "../Footer";
+import Cards from "../Cards";
 
 export default function Products() {
-  return <h1 className="products">PRODUCTS</h1>;
+  return (
+    <>
+      <img
+        alt="Travel Image"
+        src="images/img-11.jpg"
+        width="1500"
+        height="300"
+      />
+      <Cards />
+      <Footer />;
+    </>
+  );
 }
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "../../App.css";
+import Footer from "../Footer";
+import Cards from "../Cards";
 
-export default function Services() {
-  return <h1 className="services">SERVICES</h1>;
+export default function Services(props) {
+  return (
+    <>
+      <img
+        alt="Travel Image"
+        src="images/img-10.jpg"
+        width="1500px"
+        height="500px"
+      />
+      <Cards />
+      <Footer />;
+    </>
+  );
 }
